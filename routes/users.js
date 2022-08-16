2d9cc181ae96bd500a7fca837b71cf73c82f29a3
Tenzin Lekphell <61218333+tenzinlekphell@users.noreply.github.com>
2022-08-16 09:57:23 -0400

    Create user routes for REST API

---
+++ routes/users.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import {
+  getUsers,
+  getUser,
+  createUser,
+  updateUser,
+  deleteUser,
+} from '../controllers/users.js';
+
+const router = express.Router();
+
+// User routes
+router.get('/', getUsers);
+
+router.post('/', createUser);
+
+router.get('/:id', getUser);
+
+router.patch('/:id', updateUser);
+
+router.delete('/:id', deleteUser);
+
+export default router;
